4ccf2c4f49a70450630aed707c81055c0eed57c5
nimr77 <nimr.h.sawafta@gmail.com>
2022-05-25 13:10:28 +0100

    prisma

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { PrismaController } from './prisma.controller';
+
+@Module({
+  providers: [PrismaService],
+  controllers: [PrismaController]
+})
+export class PrismaModule {
+    
+}
