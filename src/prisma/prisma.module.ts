002f034bb19b27837b03ef7fb32d3c7aee32c7df
nimr77 <nimr.h.sawafta@gmail.com>
2022-05-25 13:12:04 +0100

    prisma model

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { PrismaClient } from '@prisma/client';
 import { PrismaController } from './prisma.controller';
+import { PrismaService } from './prisma.service';
 
 @Module({
   providers: [PrismaService],
   controllers: [PrismaController]
 })
-export class PrismaModule {
+export class PrismaModule  extends PrismaClient{
     
 }
